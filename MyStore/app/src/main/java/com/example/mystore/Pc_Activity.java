857bc45ee1cb2fa72a098b2b74eba166c07c4452
LeonardoRenoldi <futebol668@gmail.com>
2023-10-01 23:35:49 -0300

    BugFixes

---
+++ MyStore/app/src/main/java/com/example/mystore/Pc_Activity.java
@@ -0,0 +1,28 @@
+package com.example.mystore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pc_Activity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pc);
+
+    }
+
+
+}
--- MyStore/app/src/main/java/com/example/mystore/ShopActivity.java
+++ MyStore/app/src/main/java/com/example/mystore/ShopActivity.java
@@ -1,12 +1,19 @@
 package com.example.mystore;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 public class ShopActivity extends AppCompatActivity {
+ImageButton paginaPc, paginaPhone, paginaAc;
+
+
 
 
 
@@ -16,6 +23,25 @@ public class ShopActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+        paginaPc = (ImageButton) findViewById(R.id.pc_page) ;
+        paginaPhone= (ImageButton) findViewById(R.id.phone_page);
+        paginaAc = (ImageButton) findViewById(R.id.ac_page);
+
+        paginaPc.setOnClickListener(v -> {
+            Intent intent = new Intent(ShopActivity.this, Pc_Activity.class);
+            startActivity(intent);
+        });
+
+        paginaPhone.setOnClickListener(v -> {
+            Intent intentP = new Intent(ShopActivity.this, Iphone_Activity.class);
+            startActivity(intentP);
+        });
+
+        paginaAc.setOnClickListener(v -> {
+            Intent intentAc = new Intent(ShopActivity.this, Ac_Activity.class);
+            startActivity(intentAc);
+        });
 
     }
+
 }
